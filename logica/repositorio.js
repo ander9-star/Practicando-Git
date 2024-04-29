f81c7eaadc2e7e41a25f566deac58416654443f0
Miyhon12 <johncarnero@gmail.com>
2024-04-29 10:50:20 -0500

    se ha modifciado el archivo .js

--- logica/repositorio.js
+++ logica/repositorio.js
@@ -1,3 +1,4 @@
-document.getElementById("boton").addEventListener("click", () => {
+d = document.getElementById("boton").addEventListener("click", () => {
     console.log("hola");
+    console.log("Desde la consola web")
 })  

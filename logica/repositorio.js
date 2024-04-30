a089415e2c75b822b448339b0e79ba333fc1bc99
Miyhon12 <johncarnero@gmail.com>
2024-04-30 10:07:05 -0500

    he modificado el archivo index.html, javascript y vscode

--- logica/repositorio.js
+++ logica/repositorio.js
@@ -2,3 +2,7 @@ d = document.getElementById("boton").addEventListener("click", () => {
     console.log("hola");
     console.log("Desde la consola web")
 })  
+
+function saludar() {
+    alert("Hola mundo");
+}
